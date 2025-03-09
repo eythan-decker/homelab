6f86248aa68cae56c143510b71b49b9163b84e18
Eythan Decker <eythandecker@gmail.com>
2025-03-09 16:01:46 -0400

    updating desc for ella class vm. planning to use for another k3s node for storage

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ module "ella_class_vms" {
   vm_class_name = "pve-ella"
   vm_template_name = "ubuntu-server-noble"
   proxmox_node_name = "pve01"
-  vm_desc = ["Ella Class VM. k3s master1", "Ella Class VM. k3s master2", "Ella Class VM. k3s master3", "Ella Class VM. Rancher Admin Cluster"]
+  vm_desc = ["Ella Class VM. k3s master1", "Ella Class VM. k3s master2", "Ella Class VM. k3s master3", "Ella Class VM. Critical K3s worker node non HA."]
   vm_cpu_cores = 4
   vm_memory = 4096
   vm_serveradmin_password = var.serveradmin_password
